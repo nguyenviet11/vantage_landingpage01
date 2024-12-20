c3bb3d5f0421fac07f84c8ec3474f458fd17d438
vietnv-mmo <nguyenvanvietbn1998@gmail.com>
2024-12-20 15:35:01 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,45 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div>
+      <div className={styles.container}>
+        <div className={styles.mainMenu}>
+          <div className={`${styles.inner}`}>
+            <a
+              href="https://www.vantagetradings.com/open-live-account/?affid=MTUwNzQ2"
+              target="_blank"
+            >
+              <div className={styles.wrapperImg}></div>
+            </a>
+            <div className={styles.wrapperBtn}>
+              <div className={styles.btnSuccess}>
+                <a
+                  href="https://www.vantagetradings.com/open-live-account/?affid=MTUwNzQ2"
+                  target="_blank"
+                >
+                  詳しくはこちら
+                </a>
+              </div>
+              <div className={styles.btnCancel}>
+                <a
+                  href="https://www.vantagetradings.com/open-live-account/?affid=MTUwNzQ2"
+                  target="_blank"
+                >
+                  クイック口座開設
+                </a>
+              </div>
+            </div>
+            <div className={styles.footer}>
+              <h4>会社情報</h4>
+              <p className={styles.title}>
+                会社名：Vantage Prime Trading Limited.
+              </p>
+              <p>住所: 10 Manoel Street , Castries , St.Lucia</p>
+              <p>連絡先: support@vantagetradings.com</p>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
